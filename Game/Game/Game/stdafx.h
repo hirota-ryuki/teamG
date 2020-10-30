edbc37c66dce70ff2ee33f333fd96244ac500c2c
kbc18b24 <kbc18b24@stu.kawahara.ac.jp>
2020-10-30 12:03:12 +0900

    start

---
+++ Game/Game/Game/stdafx.h
@@ -0,0 +1,7 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+//インクルードディレクトリを追加することで余計なファイルパスの記述を省くことができる。
+//参照。
+//https://qiita.com/akurobit/items/b6c05723c407b6bfd7f1
+#include "HiroEngine.h"
+
---
+++ Game/HiroEngine/math/Math.cpp
@@ -0,0 +1,8 @@
+/*!
+ * @brief	数学関係色々。
+ */
+#include "HiroEngine/tkEnginePreCompile.h"
+#include "HiroEngine/math/tkMath.h"
+
+namespace tkEngine{
+}
