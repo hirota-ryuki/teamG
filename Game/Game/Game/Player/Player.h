cff02fc52f0893641ff56525b9c3dc9b3d72151c
Kit1145114 <kbc18b23@stu.kawahara.ac.jp>
2020-11-17 13:21:00 +0900

    PlayerとGameCamera処理追加しました。

--- Game/Game/Game/Player/Player.h
+++ Game/Game/Game/Player/Player.h
@@ -39,29 +39,26 @@ private:
 	/// プレイヤーのコントローラー入力の処理。
 	/// </summary>
 	void PlayerInputProcessing();
+	/// <summary>
+	/// プレイヤーの回転処理。
+	/// </summary>
+	void PlayerRotation();
 private:
-	//プレイヤーのモデル生成する情報握ります。
-	ModelInitData playerInitData;
-	//プレイヤーのモデル。
-	SkinModelRender* playerModel = nullptr;
-	//プレイヤーのポジション
-	Vector3 m_position = Vector3::Zero;
-	//プレイヤーのスピードを扱う
-	Vector3 m_moveSpeed = Vector3::Zero;
-	//カメラ前方方向と右方向取得用。
-	Vector3 cameraForward = Vector3::Zero;
-	Vector3 cameraRight = Vector3::Zero;
-	//回転
-	Quaternion m_rotation = Quaternion::Identity;
-	//大きさ
-	Vector3 m_scale = {1.0f,1.0f,1.0f};
-	//移動速度。
-	float m_speed = 10.0f;
-	//走るときに速さ掛ける変数。
-	int m_runSpeed = 1;
-	//マジックナンバーを防ぐゼロ。
-	const float ZERO = 0.0f;		
-	CharacterController m_charaCon;								//キャラクターコントローラー。
+	SkinModelRender* playerModel = nullptr;			//プレイヤーのモデル。
+	CharacterController m_charaCon;					//キャラクターコントローラー。
 
+	Vector3 m_position = Vector3::Zero;				//プレイヤーのポジション。
+	Vector3 m_moveSpeed = Vector3::Zero;			//プレイヤーのスピードを扱う。
+	Vector3 cameraForward = Vector3::Zero;			//移動に使うカメラ前方方向取得。
+	Vector3 cameraRight = Vector3::Zero;			//移動に使うカメラ左右方向取得。
+	Vector3 m_scale = { 1.0f,1.0f,1.0f };			//大きさ。
+
+	Quaternion m_rotation = Quaternion::Identity;	//主に使う回転の変数。
+	Quaternion m_rotating = Quaternion::Identity;	//モデルが動いてるとき使う回転変数。
+	
+	const float ZERO = 0.0f;						//マジックナンバーを防ぐゼロ。
+	float m_speed = 10.0f;							//移動速度。
+		
+	int m_runSpeed = 1;								//走るときに速さ掛ける変数。
 };
 
--- Game/Game/Game/camera/GameCamera.cpp
+++ Game/Game/Game/camera/GameCamera.cpp
@@ -1,9 +1,9 @@
 #include "stdafx.h"
 #include "GameCamera.h"
 #include"Player/Player.h"
+#include"Game.h"
 GameCamera::GameCamera()
 {
-
 }
 
 GameCamera::~GameCamera()
@@ -13,19 +13,89 @@ GameCamera::~GameCamera()
 
 bool GameCamera::Start()
 {
-	//カメラ
-	//m_target = m_player->GetPos();
-	m_target.y += 50.0f;
+	Game* game = Game::GetGame();
+	m_player = game->GetPlayer();
+
+	m_cameraState = toPlayer;
 	return true;
 }
 
 void GameCamera::Update()
 {
-	m_position = m_playerPos + m_toPos;
+	StateCamera();
+	//後で消えるよ。
+	if (g_pad[0]->IsTrigger(enButtonA))
+	{
+		isCameraTagChange_flag = true;
+	}
+}
+//カメラが動く処理。
+void GameCamera::MoveCamera()
+{
+	const float m_targetUpY = 50.0f;
+	//カメラ
+	m_target.y += m_targetUpY;
+	m_position = m_player->GetPos() + m_toPos;
+	m_target = m_player->GetPos();
+
 	//視点
 	g_camera3D->SetTarget(m_target);
 	//座標
 	g_camera3D->SetPosition(m_position);
 	//カメラの更新。
 	g_camera3D->Update();
+}
+//カメラの動かないときの処理。
+void GameCamera::StopCamera()
+{
+	//指定の場所を入力。
+	m_position = m_position1;
+	m_target = m_target1;
+
+	g_camera3D->SetTarget(m_target);
+	g_camera3D->SetPosition(m_position);
+	g_camera3D->Update();
+}
+//カメラの視点変更
+void GameCamera::ChangePointOfView()
+{
+	if (isCameraTagChange_flag /*&& 指定場所の条件 */)
+	{
+		m_cameraState = toPosition1;
+		/*//時間経過でfalseに
+		if (m_timer > m_limitTime)
+		{
+			m_cameraState = toPlayer;
+			isCameraTagChange_flag = false;
+		}*/
+	}
+	else if (isCameraTagChange_flag && !isCameraTagChange_flag/*&& 指定場所の条件 */)
+	{
+		m_cameraState = toPosition2;
+		/*//時間経過でfalseに
+		if (m_timer > m_limitTime)
+		{
+			m_cameraState = toPlayer;
+			isCameraTagChange_flag = false;
+		}*/
+	}
+}
+//状態変化
+void GameCamera::StateCamera()
+{
+	switch (m_cameraState)
+	{
+	case GameCamera::toPlayer:
+		ChangePointOfView();
+		MoveCamera();
+		break;
+	case GameCamera::toPosition1:
+		ChangePointOfView();
+		StopCamera();
+		break;
+	case GameCamera::toPosition2:
+		ChangePointOfView();
+		StopCamera();
+		break;
+	}
 }
