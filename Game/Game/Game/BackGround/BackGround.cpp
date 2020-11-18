d23b4b9aeae9245a342fc327d3a66026f031f67c
Kit1145114 <kbc18b23@stu.kawahara.ac.jp>
2020-11-18 11:09:24 +0900

    文字化け訂正

--- Game/Game/Game/BackGround/BackGround.cpp
+++ Game/Game/Game/BackGround/BackGround.cpp
@@ -25,7 +25,6 @@ void BackGround::Update()
 void BackGround::InitModel()
 {
 	m_bg = NewGO<SkinModelRender>();
-	//���f���h���[���Ă܂��B
 	m_bg->Init("Assets/modelData/background/Model.tkm");
 	m_bg->SetPos(m_position);
 }
